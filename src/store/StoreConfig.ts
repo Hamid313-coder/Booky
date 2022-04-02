46c5711f0fc2b68465ecfa9dc26cda5cf15aef38
Hamid313-Coder <hamid.hussainy0@gmail.com>
2022-04-02 08:19:40 +0430

    created new branch for redux toolkit

--- src/store/StoreConfig.ts
+++ src/store/StoreConfig.ts
@@ -4,6 +4,11 @@ import thunk from "redux-thunk";
 import AsyncStorageLib from "@react-native-async-storage/async-storage";
 import { persistStore, persistReducer } from "redux-persist";
 
+//In this root reducer just the book marks are saved
+//So we know from this result that there are some data (except the ones which are fetched from the server), that needs
+//to be saved in order to achive them  after reopening the app
+//like book marks 😄
+
 const presistConfig = {
   key: "root",
   storage: AsyncStorageLib,
